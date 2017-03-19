67c703b6e4870ec227c40ad4d8ed780c58f2343a
Zielony <niewartotupisac@gmail.com>
2017-03-20 00:09:14 +0100

    initial commit

---
+++ app/src/main/java/tk/zielony/landscapeview/MainActivity.java
@@ -0,0 +1,13 @@
+package tk.zielony.landscapeview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
